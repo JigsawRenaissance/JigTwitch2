f1d032e4d1ffa6fd4cba877cdd897b289e7e2eb7
Isa  F <BlkDragon96@gmail.com>
2012-03-12 14:59:27 -0700

    Switch logic overhaul; update to EthernetClient library.
    - Reduced the denounce logic to make it easier to understand.
    - Updated the Ethernet Library calls to reflect the most recent
    inclusion of DHCP functionality/Client use
    - postToTwitter still a WIP, needs better error handling/verbosity

--- Firmware/Twitch/Twitch.ino
+++ Firmware/Twitch/Twitch.ino
@@ -1,5 +1,5 @@
 /*
-November 26th 2011
+March 12th 2012
 Twitch by Isa Farnik
 Arduino based, tweeting light switch. For use in Jigsaw Renaissance (@Jigsawseattle).
 This source from: https://github.com/JigsawRenaissance/JigTwitch2
@@ -7,63 +7,57 @@ This source from: https://github.com/JigsawRenaissance/JigTwitch2
 Required Hardware:
 1x Arduino Uno/SMD or Arduino Duemilanove w/ ATmega328
 1x Arduino Ethernet Shield featureing the Wiznet 5100 chip
-2x Houshold light switches
-1x Plastic or otherwise 
+1x Houshold light switches
 1x Appropriate length of terminated Cat5 cabling
 1x Barrel negative power supply w/ at least 500mA of output
 
 Hardware Description:
-The first contact of both switches is connected to the Arduino's Gnd. The other contact,
-connects both to one of the Arduino's digital I/O pins denoted by the variables switch0Pin
-& switch1Pin, as well as to a pull up/down resistor and which is then also connected to Gnd.
+The first contact of the switch is connected to the Arduino's Gnd. The other contact,
+connects both to one of the Arduino's digital I/O pins denoted by the variables switchPin,
+as well as to a pull up/down resistor and which is then also connected to Gnd.
 
 More info at http://wiki.jigren.org/Twitch
 */
 
-#if defined(ARDUINO) && ARDUINO > 18   // Arduino 0019 or later
-  #include <SPI.h>
-#endif
 #include <Ethernet.h>
-#include <EthernetDHCP.h> // DHCP library via gkaindl.com/software/arduino-ethernet/dhcp
+#include <SPI.h>
 #include <avr/pgmspace.h>
 
-byte mac[] = { 0x4A, 0x69, 0x67, 0x73, 0x61, 0x77 }; // "Jigsaw" in Hex
+byte mac[] = { 0x4A, 0x69, 0x67, 0x73, 0x61, 0x77 }; // Jigsaw in Hex for the MAC address
+char alixsysURL[] = "twitter.alixsys.com"; // twitter.alixsys.com/ 92, 243, 14, 243 IP of simpleAuth proxy service
+IPAddress jigTwitchIP(192, 168, 0, 77);
 
-byte alixsysIP[] = { 92, 243, 14, 243 };        // simpleauth -> Oauth proxy twitter.alixsys.com/ 92.243.14.243
-//byte server[] = { 72, 2, 118, 214 };          // "                       " api.supertweet.net 72.2.118.214
+const int switchPin = 8; // Light switch connected to pin 8
+int switchState = digitalRead(switchPin); // Initial state of switch (off)
 
+int reading = 0; // Used later during debounce code block
 
+long lastDebounceTime = 0; // time in ms since last checked for debounce
+long debounceDelay = 2500; // amount of time in ms (2.5 sec) to wait before sending off tweet
 
-const int switch0Pin = 8;        //Switch attached to pin 8
+EthernetClient client;
 
-int switch0State = 0;
-int reading = 0;
+const int tweetCount = 6; // total number of tweet strings, both on & off
+int randomNumber = 42; // initialize random number
+char buffer[200]; // character buffer used later
 
-long lastDebounceTime = 0;
-long debounceDelay = 2500;
+//char* debugTweet = " \\&_~!@#$%^&*()+QWERTYUIOP{}|[]-=`;:?/Well you made it this far..";
 
-Client twitter(alixsysIP, 80);          // Client object for alixsys oauth proxy on port 80
+char* alixsysToken = "4e49c13d5a99d";  //4e49c13d5a99d:debugTwitch
+                                       //4d03053671de7:JigTwitch
 
-const int tweetCount = 6;               // Total number of tweets
-
-int randomNumber = 42;
-
-char buffer[200];                       // Buffer later used for strings
-
-
-char* alixsysToken = "4d03053671de7"; //4e49c13d5a99d:debugTwitch - Tokens for the alixsys proxy for 2 twitter accounts
-                                      //4d03053671de7:JigTwitch
-
-prog_char *tweetStrings[2][tweetCount] = { // Tweet strings saved into the Arduino's flash (PROGram) MEMory, instead of SRAM
+prog_char *tweetStrings[2][tweetCount] = {
+//{"#closed","Not Open. #closed","Door's locked. #closed","Nope. #closed","Cerrado. #closed","Time to go home for dinner. #closed","Nobody's here!. #closed","I'm sorry, I can't do that Dave. #closed","Portal Closed. #closed","Shut. #closed","Worm Hole collapsed. #closed","Le Clos-ed. #closed",   }  ,  {"Coffee's on. #open","#open","Come on over! #open","Yes, We're open. #open","Someone is here #open","Door is unlocked. #open","Abierto. #open","Come in and play! #open","Openinsky. #open","Portal Open. #open","Worm hole detected. #open","Ooooooooooooopen. #open"}
+//{"%23closed","Not%20Open.%20%23closed","Door%27s%20locked.%20%23closed","Nope.%20%23closed","Cerrado.%20%23closed","Time%20to%20go%20home%20for%20dinner.%20%23closed","Nobody%27s%20here%21.%20%23closed","I%27m%20sorry%2C%20I%20can%27t%20do%20that%20Dave.%20%23closed","Portal%20Closed.%20%23closed","Shut.%20%23closed","Worm%20Hole%20collapsed.%20%23closed","Le%20Clos-ed.%20%23closed"}};
 {
-    "%23closed",            // URL escaped to save on space required by URL escaping function
+    "%23closed",
     "Not%20Open.%20%23closed",
     "Door%27s%20locked.%20%23closed",
     "Nope.%20%23closed",
     "Cerrado.%20%23closed",
     "Time%20to%20go%20home%20for%20dinner.%20%23closed"}
   ,
-  {                         // 2-D array loaded w/ tweet strings, col 0 as off, col 1 as on
+  {
     "Coffee%27s%20on.%20%23open",
     "%23open",
     "Come%20on%20over%21%20%23open",
@@ -73,47 +67,76 @@ prog_char *tweetStrings[2][tweetCount] = { // Tweet strings saved into the Ardui
 };
 
 void setup() {
-  delay(1000);              
-  randomSeed(analogRead(0));            // required to keep random() different
-  pinMode(switch0Pin, INPUT);
-  EthernetDHCP.begin(mac);              // initialize dhcp
+  delay(1000);
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+
+  pinMode(switchPin, INPUT);
+
+  if (!Ethernet.begin(mac)) {
+    // if DHCP fails, start with a hard-coded address:
+    Ethernet.begin(mac, jigTwitchIP);
+  }
+
+  Serial.print("[DHCP] ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+
   randomNumber = random(0, (tweetCount-1));
+  Serial.print("[RAND] New random number: ");
+  Serial.println(randomNumber);
 }
 
 void loop() { 
-  if (lastDebounceTime == 0) {          // determine if debounce has been accounted for recently
-    reading = digitalRead(switch0Pin);
-    if (reading != switch0State) {
-      lastDebounceTime = millis();      // begin debounce timer if switch flipped
+  if (lastDebounceTime == 0) {
+    reading = digitalRead(switchPin);
+    if (reading != switchState) {
+      lastDebounceTime = millis();
     };
   } 
   else if (((millis() - lastDebounceTime) >= debounceDelay) || (millis() < lastDebounceTime)) {
-    reading = digitalRead(switch0Pin);
-    if (reading != switch0State) {              // if switch flipped since last checked
-      randomNumber = random(0, (tweetCount-1));          // pick a random rumber to use as the tweet array index
-      switch (reading) {                    // use switch state t determine which type of tweet
-      case HIGH:
+    reading = digitalRead(switchPin);
+    if (reading != switchState) {
+        Serial.print("[SWCH] Toggled, now ");
+        Serial.print(reading);
+        Serial.println(".");
         postToTwitter(tweetStrings[reading][randomNumber]);
-        break;
-      case LOW:
-        postToTwitter(tweetStrings[reading][randomNumber]);
-        break;
-      }
     }
-    switch0State = reading;            // save temp switch state as default
-    lastDebounceTime = 0;              // reset debounce timer
-  }
-  EthernetDHCP.maintain();             // maintain the DHCP lease
-} 
-
-
-void postToTwitter(char* tweet) {
-  sprintf(buffer,"GET /update/?axk=%s&status=%s HTTP/1.1\nHost: twitter.alixsys.com\n\n",alixsysToken,tweet);  // use sprintf to concatanate HTTP request string
-  if (twitter.connect()) {        // open connection to alixsys
-    twitter.print(buffer);        // print HTTP
-  }
-    twitter.stop();
-  } else if (!twitter.connect()) {        // must impliment better error handling..
-    twitter.stop();
+    switchState = reading;
+    lastDebounceTime = 0;
   }
 }
+
+void postToTwitter(char *tweet) {
+  sprintf(buffer,"GET /update/?axk=%s&status=%s HTTP/1.1\nHost: twitter.alixsys.com\n\n",alixsysToken,tweet);
+  Serial.print("[TWIT] buffer: ");
+  Serial.println(buffer);
+  
+  Serial.println("[HTTP] Connecting...");
+  if (client.connect(alixsysURL, 80)) {
+    Serial.println("[HTTP] Connected, sending HTTP GET");
+    client.print(buffer);
+    
+    delay(debounceDelay);
+    Serial.println("[HTTP] Checking for response...");
+    if (client.available()) {
+      Serial.println("[HTTP] Response found, printing.");
+      char c = client.read();
+      Serial.print(c);
+      
+      client.flush();
+      Serial.println();
+    } else {
+      Serial.println("[HTTP] No response.");
+    }
+    client.stop();
+  } else {
+    Serial.println("[HTTP] Did not connect.");
+  }
+  Serial.println("[HTTP] Stopping connection.");
+}
+
